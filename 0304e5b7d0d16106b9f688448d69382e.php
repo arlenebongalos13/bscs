8c1fb56e1e377a2d82f5fec8e4271e7530481852
arlenebongalos13 <arlenebongalos63@gmail.com>
2024-10-06 19:20:16 +0800

    Add files via upload
    commit

---
+++ 0304e5b7d0d16106b9f688448d69382e.php
@@ -0,0 +1,58 @@
+
+  
+<?php $__env->startSection('title', 'Home'); ?>
+  
+<?php $__env->startSection('contents'); ?>
+    <div class="d-flex align-items-center justify-content-between">
+        <h1 class="mb-0">List of Residence</h1>
+        <a href="<?php echo e(route('baranggay.create')); ?>" class="btn btn-primary">Add Residence</a>
+    </div>
+    <hr />
+    <?php if(Session::has('success')): ?>
+        <div class="alert alert-success" role="alert">
+            <?php echo e(Session::get('success')); ?>
+
+        </div>
+    <?php endif; ?>
+    <table class="table table-hover">
+        <thead class="table-primary">
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Birthday</th>
+                <th>Purok</th>
+                <th>Age</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>+
+            <?php if($baranggay->count() > 0): ?>
+                <?php $__currentLoopData = $baranggay; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rs): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <tr>
+                        <td class="align-middle"><?php echo e($loop->iteration); ?></td>
+                        <td class="align-middle"><?php echo e($rs->name); ?></td>
+                        <td class="align-middle"><?php echo e($rs->birthday); ?></td>
+                        <td class="align-middle"><?php echo e($rs->purok); ?></td>
+                        <td class="align-middle"><?php echo e($rs->age); ?></td> 
+                        <td class="align-middle">
+                            <div class="btn-group" role="group" aria-label="Basic example">
+                                <a href="<?php echo e(route('baranggay.show', $rs->id)); ?>" type="button" class="btn btn-secondary">Detail</a>
+                                <a href="<?php echo e(route('baranggay.edit', $rs->id)); ?>" type="button" class="btn btn-warning">Edit</a>
+                                <form action="<?php echo e(route('baranggay.destroy', $rs->id)); ?>" method="POST" type="button" class="btn btn-danger p-0" onsubmit="return confirm('Delete?')">
+                                    <?php echo csrf_field(); ?>
+                                    <?php echo method_field('DELETE'); ?>
+                                    <button class="btn btn-danger m-0">Delete</button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            <?php else: ?>
+                <tr>
+                    <td class="text-center" colspan="5">Person not found</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('components.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Kaiserr\Desktop\baranggay1\resources\views/baranggay/index.blade.php ENDPATH**/ ?>
---
+++ b643bd0084280a706c7ddd1359b45a13.php
@@ -0,0 +1,34 @@
+
+  
+<?php $__env->startSection('title', 'Add'); ?>
+  
+<?php $__env->startSection('contents'); ?>
+    <h1 class="mb-0">Add Residence</h1>
+    <hr />
+    <form action="<?php echo e(route('baranggay.store')); ?>" method="POST" enctype="multipart/form-data">
+        <?php echo csrf_field(); ?>
+        <div class="row mb-3">
+            <div class="col">
+                <input type="text" name="name" class="form-control" placeholder="Name">
+            </div>
+            <div class="col">
+                <input type="text" name="birthday" class="form-control" placeholder="Birthday">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col">
+                <input type="text" name="purok" class="form-control" placeholder="Purok">
+            </div>
+            <div class="col">
+                <input type="text" name="age" class="form-control" placeholder="Age">
+            </div>
+        </div>
+ 
+        <div class="row">
+            <div class="d-grid">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('components.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Kaiserr\Desktop\baranggay1\resources\views/baranggay/create.blade.php ENDPATH**/ ?>
